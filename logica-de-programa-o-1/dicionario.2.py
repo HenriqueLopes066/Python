4595ee40205567e388fc375ea75c04df30bf9081
Henrique <henriquelopis066@gmail.com>
2025-09-17 16:33:22 -0300

    Add files via upload

---
+++ logica-de-programa-o-1/dicionario.2.py
@@ -0,0 +1,10 @@
+clientes = []
+cliente = {}
+
+cliente["nome"] = input("digite seu nome: ")
+cliente["telefone"] = input("digite seu telefone: ")
+cliente["email"] = input("digite seu email: ")
+cliente["profissao"] = input("digite sua profissao: ")
+print(cliente)
+
+
