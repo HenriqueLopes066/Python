e4ab7dea9b501bc5502bfb6a226f38c6346b1223
Henrique <henriquelopis066@gmail.com>
2026-06-16 14:12:49 -0300

    Create controle_de_entrada.py

---
+++ Atividade de sala/controle_de_entrada.py
@@ -0,0 +1,34 @@
+print("=====================================")
+print("   Controle de Entrada de um evento  ")
+print("=====================================")
+
+
+pessoa_perm = 0
+pessoa_negada = 0
+
+while True:
+
+    nome = str(input("\nDigite seu nome: "))
+
+    idade = int(input("\nDigite sua idade: "))
+
+    if idade >= 18:
+        print("Entrada permitida!")
+        pessoa_perm += 1
+        
+
+    elif idade <18:
+        print("Entrada negada!")
+        pessoa_negada += 1
+        
+
+    else:
+        print("Idade inválida, tente novamente!")
+    
+    continuar = str(input("\n Deseja continuar? (s/n): "))
+    print("=====================================")
+    if continuar == "n":
+        break
+
+print(" Total de pessoas que foram negadas: ", pessoa_negada)
+print(" Total de pessoas que entraram: ", pessoa_perm)  
